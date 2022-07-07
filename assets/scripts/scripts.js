f952082e8ae6259b570120f74f465475280840fb
Matias Di Iorio <matiasmd8610@gmail.com>
2022-07-06 21:10:07 -0300

    First Commit

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,5 @@
+/* scroll menu  */
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+  })
